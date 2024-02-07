cfbcce1709e5d295d301c8bd85653696d0cd4f73
fabiobnm <ffabiobnm@gmail.com>
2024-02-07 15:15:41 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -140,7 +140,7 @@ export default function Blog() {
         priority={true} // Se stai usando la funzionalità di rendering di priorità di Next.js
         unoptimized={false} // Se non hai bisogno di ottimizzazione automatica
         onError={handleLogoError}
-          alt="TOGNON"
+          alt={"TOGNON"}
         />
       </Link>
 
