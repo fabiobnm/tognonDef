55e7ef958c53fbb4675457de87ac682dc14c0840
fabiobnm <ffabiobnm@gmail.com>
2024-03-29 22:12:20 +0100

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -134,8 +134,6 @@ export default function Blog() {
    <Image class="projCover"
         src={prog.cover?.url ||  prog.gallery[0].url}
         alt="Description of the image"
-        width={300} // larghezza dell'immagine
-        height={200} // altezza dell'immagine
         priority={true} // Se stai usando la funzionalità di rendering di priorità di Next.js
         unoptimized={false} // Se non hai bisogno di ottimizzazione automatica
         onError={handleLogoError}
