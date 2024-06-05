4cda161d709ed8c62f496b70aebf11d6249fc5a6
fabiobnm <ffabiobnm@gmail.com>
2024-06-05 23:48:04 +0200

    Update collectible.js

--- src/pages/collectible.js
+++ src/pages/collectible.js
@@ -134,8 +134,6 @@ export default function Blog() {
       <Image class="projCoverPort" 
       src={prog.cover?.url ||  prog.gallery[0].url}
         alt="Description of the image"
-        height={50}
-        width={50}
         priority={true} // Se stai usando la funzionalità di rendering di priorità di Next.js
         unoptimized={false} // Se non hai bisogno di ottimizzazione automatica
         onError={handleLogoError}
