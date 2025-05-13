d64c28e0370b3bb4ab6f5f4bd5ec11ba387cba53
fabiobnm <ffabiobnm@gmail.com>
2025-05-13 16:07:32 +0200

    Update next.config.js
    change next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-
-module.exports = {
   images: {
     domains: ['media.graphassets.com'],
+    minimumCacheTTL: 2678400, // 31 giorni
   },
 };
+
+module.exports = nextConfig;
